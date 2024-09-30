7db98777b4dd2858b69ebd9a0034444fd961baef
santtose <santtose@outlook.com>
2024-09-30 14:01:05 -0300

    Add contexto

---
+++ src/DevIO.Data/Context/MeuDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DevIO.Business.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevIO.Data.Context
+{
+    public class MeuDbContext : DbContext
+    {
+        public MeuDbContext(DbContextOptions<MeuDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Fornecedor> Fornecedores { get; set; }
+    }
+}
--- src/DevIO.Data/obj/Debug/net8.0/DevIO.Data.AssemblyInfo.cs
+++ src/DevIO.Data/obj/Debug/net8.0/DevIO.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevIO.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+487e62125bf85c2fd7f07e97f11f649db03d36b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb28e49036b0b4dcf0eef5b13151b543b877997f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevIO.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevIO.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
