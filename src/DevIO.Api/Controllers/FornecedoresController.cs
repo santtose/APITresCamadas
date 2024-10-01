beb89e392185e77e424694167de0bfad15618641
santtose <santtose@outlook.com>
2024-10-01 10:01:17 -0300

    Implementando controllers

---
+++ src/DevIO.Api/Controllers/FornecedoresController.cs
@@ -0,0 +1,44 @@
+﻿using DevIO.Api.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/fornecedores")]
+    public class FornecedoresController : MainController
+    {
+        public FornecedoresController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<FornecedorDTO>> ObterTodos()
+        {
+            return null;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<FornecedorDTO>> ObterPorId(Guid id)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<FornecedorDTO>> Adicionar(FornecedorDTO fornecedorViewModel)
+        {
+            return null;
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<ActionResult<FornecedorDTO>> Atualizar(Guid id, FornecedorDTO fornecedorViewModel)
+        {
+            return null;
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<FornecedorDTO>> Excluir(Guid id)
+        {
+            return null;
+        }
+    }
+}
---
+++ src/DevIO.Api/Controllers/ProdutosController.cs
@@ -0,0 +1,43 @@
+﻿using DevIO.Api.DTO;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [Route("api/produtos")]
+    public class ProdutosController : MainController
+    {
+        public ProdutosController()
+        {
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<ProdutoDTO>> ObterTodos()
+        {
+            return null;
+        }
+
+        [HttpGet("{id:guid}")]
+        public async Task<ActionResult<ProdutoDTO>> ObterPorId(Guid id)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<ProdutoDTO>> Adicionar(ProdutoDTO produtoViewModel)
+        {
+            return null;
+        }
+
+        [HttpPut("{id:guid}")]
+        public async Task<IActionResult> Atualizar(Guid id, ProdutoDTO produtoViewModel)
+        {
+            return null;
+        }
+
+        [HttpDelete("{id:guid}")]
+        public async Task<ActionResult<ProdutoDTO>> Excluir(Guid id)
+        {
+            return null;
+        }
+    }
+}
